bb4865d3a48e45ef517a33a75156ce31d9ec0a3b
Fanuel Sohilait <nuelsohilait@gmail.com>
2022-01-14 12:50:22 +0900

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nulla esse vero distinctio voluptates commodi, aperiam optio odit perferendis nisi autem sequi atque libero sint obcaecati in? Sit, doloremque perferendis?</p>
 </body>
 </html>
